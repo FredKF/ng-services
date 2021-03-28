f7a1dc925155676fb930c02c62503d35e7d00571
FredKF <fedetkd86@hotmail.com>
2021-03-28 14:33:16 +0200

    refactor

---
+++ src/app/models/character.model.ts
@@ -0,0 +1,31 @@
+export interface Character {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: Origin;
+    location: Location;
+    image: string;
+    episode: string[];
+    url: string;
+    created: Date;
+}
+
+interface Info {
+    count: number;
+    pages: number;
+    next: string;
+    prev?: any;
+}
+
+interface Origin {
+    name: string;
+    url: string;
+}
+
+interface Location {
+    name: string;
+    url: string;
+}   
