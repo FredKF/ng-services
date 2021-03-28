5c9647662f759ac02e7a57f279165e0a6caabd5a
FredKF <fedetkd86@hotmail.com>
2021-03-28 14:52:34 +0200

    API call

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from 'src/app/models/character.model';
+import { CharacterService } from 'src/app/services/character.service';
 
 @Component({
   selector: 'app-character-list',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterListComponent implements OnInit {
 
-  constructor() { }
+  characters: Character[] = [];
+  constructor(private characterService: CharacterService ) { }
 
   ngOnInit(): void {
+    console.log('inicializado ok');
+    this.characterService.getCharacters().subscribe(
+    (res) => {
+      this.characters=res.results;
+    },
+    (error) =>{
+      console.error(error)
+    });
   }
-
 }
