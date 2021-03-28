61e11729489c17d9715409c1e891438ea8a50ce7
FredKF <fedetkd86@hotmail.com>
2021-03-28 17:24:21 +0200

    routing done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { LocationListComponent } from './components/location-list/location-list.component';
+
+const routes: Routes = [
+  {path:'characters', component: CharacterListComponent},
+  {path:'locations', component: LocationListComponent},
+  {path:'', redirectTo:'characters', pathMatch:'full'},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CharacterListComponent } from './components/character-list/character-list.component';
 import { CharacterItemComponent } from './components/character-item/character-item.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LocationListComponent } from './components/location-list/location-list.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CharacterListComponent,
-    CharacterItemComponent
+    CharacterItemComponent,
+    LocationListComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
