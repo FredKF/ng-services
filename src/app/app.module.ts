dfac89bcd5929472c5face3d880617c8016f8510
FredKF <fedetkd86@hotmail.com>
2021-03-28 16:01:31 +0200

    Input implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CharacterListComponent } from './components/character-list/character-list.component';
+import { CharacterItemComponent } from './components/character-item/character-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterListComponent
+    CharacterListComponent,
+    CharacterItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/character-item/character-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Character } from 'src/app/models/character.model';
+
+@Component({
+  selector: 'app-character-item',
+  templateUrl: './character-item.component.html',
+  styleUrls: ['./character-item.component.css']
+})
+export class CharacterItemComponent {
+
+  @Input()
+  character: Character;
+  constructor() { }
+}
